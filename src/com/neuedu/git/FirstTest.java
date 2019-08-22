19dbd1c8d0cbb3cd2c7566c23f5496fe3a2e9fd2
Mengt <Mengt995@163.com>
2019-08-22 15:25:26 +0800

    update

--- src/com/neuedu/git/FirstTest.java
+++ src/com/neuedu/git/FirstTest.java
@@ -2,6 +2,6 @@ package com.neuedu.git;
 
 public class FirstTest {
     public static void main(String[] args) {
-        System.out.print(" init group 4");
+        System.out.print(" init group 4 mt update");
     }
 }
