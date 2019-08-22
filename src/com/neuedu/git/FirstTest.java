19c9c14152cdd03d38d5a68027e533eceae14c19
wyj <876968412@qq.com>
2019-08-22 15:27:53 +0800

    init group 4

--- src/com/neuedu/git/FirstTest.java
+++ src/com/neuedu/git/FirstTest.java
@@ -3,5 +3,6 @@ package com.neuedu.git;
 public class FirstTest {
     public static void main(String[] args) {
         System.out.print(" init group 4 mt update");
+        System.out.print(" second update");
     }
 }
